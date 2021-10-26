2ad4439693e10d9c3003d4b596cdd5a3d89df867
Alvin Escobar <alvincsproductions@gmail.com>
2021-10-25 19:00:36 -0500

    Initial Commit

---
+++ MovieNight/MovieNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieNightApp.swift
+//  MovieNight
+//
+//  Created by Alvin Escobar on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
